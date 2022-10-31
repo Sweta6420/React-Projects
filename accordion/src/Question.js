b8d7df1e2b73ef6ff0ce32dfe14d1b40438e913e
Sweta <93282010+Sweta6420@users.noreply.github.com>
2022-10-31 18:26:35 +0530

    update Question.js

--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title, info}) => {
   const [showInfo, setShowInfo] = useState(false)
+  const [showAnswer, setShowAnswer] = 'answer'
+  const btn = showInfo ?  'btn-minus' : 'btn-plus'
+  const question = showInfo ?  'question' : 'question'
   return(
-    <article className='question'>
+    <article className={question}>
       <header>
         <h4>{title}</h4>
-        <button className='btn' onClick={() => setShowInfo(!showInfo)}>{showInfo ? <AiOutlineMinus />:<AiOutlinePlus />}</button>
+        <button className={btn} onClick={() => 
+          {
+            setShowInfo(!showInfo)
+        }}>{showInfo ? <AiOutlineMinus />:<AiOutlinePlus />}</button>
       </header>
-      {showInfo && <p>{info}</p>}
+      <div className='{showanswer}'>
+        {showInfo && <p>{info}</p>}
+      </div>
     </article>
   )
 };
